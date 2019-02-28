d6c33aac75291c1f43d08d0702b55eaf53dec316
Vagios Vlachos <bagios234@gmail.com>
2019-02-28 13:51:35 +0200

    Add routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NewRecipeComponent } from './pages/new-recipe/new-recipe.component';
+import { EditRecipeComponent } from './pages/edit-recipe/edit-recipe.component';
+import { ShowRecipeComponent } from './pages/show-recipe/show-recipe.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'recipes', component: HomeComponent },
+  { path: 'recipes/new', component: NewRecipeComponent },
+  { path: 'recipe/edit/:id', component: EditRecipeComponent },
+  { path: 'recipe/:id', component: ShowRecipeComponent },
+  { path: '**', component: HomeComponent }
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
