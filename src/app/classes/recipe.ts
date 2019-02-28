c395b9ddab5b925e60f8d9d890db98354c477e1a
Vagios Vlachos <bagios234@gmail.com>
2019-02-28 12:42:45 +0200

    Create recipe class and interface

---
+++ src/app/classes/recipe.ts
@@ -0,0 +1,23 @@
+import { IIngredient } from '../interfaces/ingredient';
+import { IRecipe } from '../interfaces/recipe';
+
+export class Recipe implements IRecipe {
+  id: number;
+  title: string;
+  description: string;
+  serves: string;
+  imageUrl: string;
+  ingredients: IIngredient[];
+  instructions: string[];
+
+  constructor({id, title, description, serves,
+    imageUrl, ingredients, instructions}) {
+      this.id = id;
+      this.title = title;
+      this.description = description;
+      this.serves = serves;
+      this.imageUrl = imageUrl;
+      this.ingredients = ingredients;
+      this.instructions = instructions;
+  }
+}
